9f16d1dbc3de2ba97db7279f04df8f782e603bfb
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-01-07 22:26:06 -0800

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
+  constructor(food, eat) {
+    super(food, eat);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+    this.makeHoney = function() {
+      this.honeyPot++;
+    }
+    this.giveHoney = function() {
+      this.honeyPot--;
+    }
+  }
   // TODO..
 };
